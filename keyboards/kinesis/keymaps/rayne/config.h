91b3dab3d4bd4017513e5f814886e3140a3b1863
Rayne Beridon <rberidon@gmail.com>
2022-05-29 18:43:27 -0700

    miryoku: add config entries

--- keyboards/kinesis/keymaps/rayne/config.h
+++ keyboards/kinesis/keymaps/rayne/config.h
@@ -4,3 +4,14 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 175
 #define TAPPING_TOGGLE 2
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
