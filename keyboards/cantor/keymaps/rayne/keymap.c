e9bea9cb7d0b4b6bcd477ae62c7ed174007f7fed
Rayne Beridon <rberidon@gmail.com>
2023-03-24 13:19:09 -0700

    Cantor: add Tap Dance setup

--- keyboards/cantor/keymaps/rayne/keymap.c
+++ keyboards/cantor/keymaps/rayne/keymap.c
@@ -1,5 +1,20 @@
 #include QMK_KEYBOARD_H
 
+//Tap Dance Declarations
+enum {
+  TD_ESC_CAPS = 0
+};
+
+//Tap Dance Definitions
+tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
+// Other declarations would go here, separated by commas, if you have them
+};
+
+//In Layer declaration, add tap dance item in place of a key code
+//TD(TD_ESC_CAPS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
