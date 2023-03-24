eb1ec822016118ad6c0fe54f9cf3032fbc11c50e
Rayne Beridon <rberidon@gmail.com>
2023-03-24 13:14:46 -0700

    Cantor: add Rayne config.h, rules.mk

---
+++ keyboards/cantor/keymaps/rayne/rules.mk
@@ -0,0 +1,7 @@
+TAP_DANCE_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes	# Enable dynamically changing tapping term duration in ms
+
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+UNICODE_ENABLE = yes        # Unicode
