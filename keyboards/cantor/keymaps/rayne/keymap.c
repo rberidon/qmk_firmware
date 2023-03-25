a3faec4f8fc5066cd63526b1c25331bb37923185
Rayne Beridon <rberidon@gmail.com>
2023-03-24 23:20:11 -0700

    Cantor: layer 0: spacing

--- keyboards/cantor/keymaps/rayne/keymap.c
+++ keyboards/cantor/keymaps/rayne/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───────┘      └───┘
       */
     [0] = LAYOUT_split_3x6_3(
-        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                 KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        R_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                 KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                 KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+        R_LCTL,     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,       KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,       KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                   KC_LCTL,  MT(MOD_LGUI, KC_BSPC), MT(MOD_LALT, KC_ENT),          KC_SPC,   TO(1),    KC_RALT
     ),
      /*
