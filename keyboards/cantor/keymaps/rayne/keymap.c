d1dfb27438c19c5ed4c2ee0e66f7ae990e2fe34b
Rayne Beridon <rberidon@gmail.com>
2023-03-24 23:16:25 -0700

    Cantor: layer 0: add lctrl on caps key

--- keyboards/cantor/keymaps/rayne/keymap.c
+++ keyboards/cantor/keymaps/rayne/keymap.c
@@ -30,6 +30,8 @@ tap_dance_action_t tap_dance_actions[] = {
  *                                              └────────┘    └────────┘
  */
 
+#define R_LCTL  MT(MOD_LCTL, KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
@@ -47,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [0] = LAYOUT_split_3x6_3(
         KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                 KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                 KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        R_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                 KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                 KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                   KC_LCTL,  MT(MOD_LGUI, KC_BSPC), MT(MOD_LALT, KC_ENT),          KC_SPC,   TO(1),    KC_RALT
     ),
@@ -113,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [4] = LAYOUT_split_3x6_3(
     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,               KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_BSPC,
-    KC_ESC,   KC_A,     KC_R,     KC_S,     KC_T,     KC_G,               KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
+    R_LCTL,   KC_A,     KC_R,     KC_S,     KC_T,     KC_G,               KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,               KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                               KC_LCTL,  MT(MOD_LGUI, KC_BSPC), MT(MOD_LALT, KC_ENT),          KC_SPC,   TO(1),    KC_RALT
     ),
